fd74b5b13749779baeef1ef8917afc4b18fbadf7
Cuong Ngo <dinhcuongngo@gmail.com>
2025-07-29 16:09:08 +0700

    feat: index page

---
+++ app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <!-- Banner -->
+    <v-row>
+      
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h1>Welcome to Home Massage Da Nang</h1>
+        <p>Your relaxation is our priority.</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <v-btn color="primary" @click="$router.push('/services')">Explore Services</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
